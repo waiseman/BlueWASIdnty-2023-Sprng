10d3987b73e1b75ca1396c0ff934e747f4fd226a
Test USer <testex@abc.com>
2023-05-28 21:08:25 +0500

    Add project files.

---
+++ BlueWASIdnty/Server/Data/Migrations/20230524071319_UserUPdated.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlueWASIdnty.Server.Data.Migrations
+{
+    public partial class UserUPdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResgistrationNo",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResgistrationNo",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ BlueWASIdnty/Server/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlueWASIdnty.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    public string? ResgistrationNo{ get; set; }    
+    }
+}
